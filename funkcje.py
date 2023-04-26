33aa26b5978af5de33fcc5ff54dbceb78f5e9010
kkarbon <131867227+kkarbon@users.noreply.github.com>
2023-04-26 12:16:42 +0200

    Update funkcje.py

--- funkcje.py
+++ funkcje.py
@@ -1,15 +1,7 @@
 #tutaj jest nowy branch
 
-def alternatywa(x,y):
-    if x == 1 and y == 1:
-        return 1
-    elif x == 1 and y == 0:
-        return 1
-    elif x == 0 and y ==1:
-        return 1
-    elif x==0 and y==0:
-        return 0
-    else:
-        print("Elementy alternatywy nie przyjmują wartości 1 lub 0")
+SDPAFJIHVBADfhiojkn"ohgBJJPIDHBJA 
 
-print(alternatywa(a,b))
+
+
+DSJOFOASFPAHHPAFAPFJAWFJSDB
